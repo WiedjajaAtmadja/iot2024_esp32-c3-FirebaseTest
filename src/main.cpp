cb5b3363d0cf9bb86856fb7ced88a58c98422c37
Steffanus W <swiedjaja@binus.ac.id>
2024-12-11 00:29:42 +0700

    add esp id

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ DHTesp dht;
 FirebaseData fbdo;
 FirebaseConfig fbConfig;
 FirebaseData fbdoStream;
+char g_szDeviceId[30];
 
 bool WiFi_Connect(int nRetry)
 {
@@ -40,10 +41,11 @@ void onTimerSendData()
   int nStatus = digitalRead(SWITCH_STATUS);
   float humidity = dht.getHumidity();
   float temperature = dht.getTemperature();
-  Firebase.RTDB.setFloat(&fbdo, "/data/temperature", temperature);
-  Firebase.RTDB.setFloat(&fbdo, "/data/humidity", humidity);
-  Firebase.RTDB.setInt(&fbdo, "/data/switchStatus", nStatus);
-  Firebase.RTDB.setInt(&fbdo, "/data/currentMillis", millis());
+  String path = "/data/" + String(g_szDeviceId);
+  Firebase.RTDB.setFloat(&fbdo, path+"/temperature", temperature);
+  Firebase.RTDB.setFloat(&fbdo, path+"/humidity", humidity);
+  Firebase.RTDB.setInt(&fbdo, path+"/switchStatus", nStatus);
+  Firebase.RTDB.setInt(&fbdo, path+"/currentMillis", millis());
 
   Serial.printf("[%7d] Temperature: %.1f C, Humidity: %.1f %%, SwitchStatus: %d \r\n",
      millis()/1000, temperature, humidity, nStatus);
@@ -122,6 +124,7 @@ void setup() {
   pinMode(LED_RED, OUTPUT);
   pinMode(SWITCH_STATUS, INPUT_PULLUP);
   dht.setup(DHT_PIN, DHTesp::DHT22);
+  sprintf(g_szDeviceId, "esp32_%08X",(uint32_t)ESP.getEfuseMac());
   Serial.begin(115200);
   WiFi_Connect(7);
   Firebase_Init("/cmd");
